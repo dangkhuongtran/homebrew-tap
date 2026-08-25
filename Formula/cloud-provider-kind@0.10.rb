2ba806268005e52ab1f0803ee30d87892294d2f7
Dan Tran <d.k.tran@uq.edu.au>
2026-08-25 16:09:11 +1000

    Remove version declaration from cloud-provider-kind@0.10 formula

--- Formula/cloud-provider-kind@0.10.rb
+++ Formula/cloud-provider-kind@0.10.rb
@@ -2,7 +2,6 @@ class CloudProviderKindAT010 < Formula
   desc "Cloud provider for KIND clusters"
   homepage "https://kubernetes-sigs.github.io/cloud-provider-kind/"
   url "https://github.com/kubernetes-sigs/cloud-provider-kind/archive/refs/tags/v0.10.0.tar.gz"
-  version "0.10.0"
   sha256 "db9829197b1940d5cce0a39c352d9d15adb58877b82431cf2fdc0e7f874253ba"
   license "Apache-2.0"
 
